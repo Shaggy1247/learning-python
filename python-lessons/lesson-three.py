4fdfc849b6936d3af1340ed7de6d35b4565f4c89
Shaggy1247 <76541695+Shaggy1247@users.noreply.github.com>
2020-12-28 14:22:21 -0600

    finished lesson three

--- python-lessons/lesson-three.py
+++ python-lessons/lesson-three.py
@@ -1,6 +1,12 @@
 # -----------------------------------------------------------
 # Lesson three
-# Going over conditions
+# Learning about conditions
 #
 # Done , Shaggy1247
 # -----------------------------------------------------------
+
+print(2 < 3)                # True
+print('hello' == 'helo')    # False
+print('hello' != 'Hello')   # True
+print(2 - 3 + 4 > 5)        # False
+# print(False == True)        # False
