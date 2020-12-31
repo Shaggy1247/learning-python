94e68a34d5a1a4bd56d5e572be08fba8f15603c3
Shaggy1247 <76541695+Shaggy1247@users.noreply.github.com>
2020-12-31 17:30:52 -0600

    added lesson thirteen and readfile.txt

---
+++ python-lessons/lesson-thirteen.py
@@ -0,0 +1,10 @@
+# -----------------------------------------------------------
+# Lesson thirteen
+# Learning about reading text file
+#
+# Done 12/28/20, Shaggy1247
+# -----------------------------------------------------------
+
+
+file = open('readfile.txt', 'r')
+f = file.readLines()
