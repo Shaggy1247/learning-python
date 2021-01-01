058f7980c16600a0c8e0c2a349299c4acd99c0d9
Shaggy1247 <76541695+Shaggy1247@users.noreply.github.com>
2020-12-31 18:09:26 -0600

    finished lesson fourteen and added writefile.txt

---
+++ python-lessons/lesson-fourteen.py
@@ -0,0 +1,13 @@
+# -----------------------------------------------------------
+# Lesson fourteen
+# Learning about writing to a text file
+#
+# Done 12/28/20, Shaggy1247
+# -----------------------------------------------------------
+
+file = open("python-lessons/writefile.txt", "w")
+
+file.write("python\n")
+file.write("I am learning to write to a file")
+
+file.close()
--- python-lessons/lesson-thirteen.py
+++ python-lessons/lesson-thirteen.py
@@ -6,5 +6,21 @@
 # -----------------------------------------------------------
 
 
-file = open('readfile.txt', 'r')
-f = file.readLines()
+file = open('python-lessons/readfile.txt', 'r')
+f = file.readlines()
+
+print(f)
+
+newList = []
+for line in f:                          # check by line
+    if line[-1] == "\n":
+        newList.append(line[:-1])
+    else:
+        newList.append(line)
+
+print(newList)
+
+for line in f:                          # easier way to clean up
+    newList.append(line.strip())
+
+print(newList)
